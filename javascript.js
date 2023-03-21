84983481993f2e764631f0bdd959f4a898cadeea
Arjun E <87701499+arjun6736@users.noreply.github.com>
2023-03-21 15:42:35 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -27,7 +27,7 @@ function boxclicked(e){
 }
 
 const winning_combos =[
-    [0,1,2],[3,4,5],[6,7,8],[0,4,8],[3,4,6],[0,3,6],[1,4,7],[2,5,8]
+    [0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]
 ]
 function playerhaswon(){
     for (const condition of winning_combos) {
